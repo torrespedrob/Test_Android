63507eb2bf3c0e7e4652555d17c4d6d64f6d2b63
Pedro Torres <torrespedrobarba@gmail.com>
2021-10-21 10:25:30 +0200

    Segundo día

--- app/src/main/java/com/ptb/test_android/MainActivity.kt
+++ app/src/main/java/com/ptb/test_android/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ptb.test_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i("Hola mundo", "esto es un primer mensaje en consola")
+
+        val texto:TextView = findViewById(R.id.texto)
+        texto.setText("¡Bienvenidos a Android con Kotlin!")
+
+
+        val boton:Button = findViewById(R.id.boton)
+        boton.setOnClickListener{
+            texto.setText("Has pulsado el boton")
+        }
+
     }
 }

ed89b02c5151fa44a8664ec6524f1e722383c5dc
Pedro Torres <torrespedrobarba@gmail.com>
2021-10-21 08:44:27 +0200

    Initial commit

---
+++ app/src/main/java/com/ptb/test_android/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ptb.test_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("Hola mundo", "esto es un primer mensaje en consola")
+    }
+}
